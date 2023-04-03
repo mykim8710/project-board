2e73d97d1cbc3af76c8cc8469ed7c4829701a6c1
mykim <mykim8710@gmail.com>
2023-04-03 22:06:40 +0900

    #1 - data rest 테스트
    - data rest를 이용하여 자동으로 설정된 api가 정상적으로 작동하는지 확인
    - 반드시 필요한 테스트는 아니지만 공부겸 작성

---
+++ src/test/java/io/mykim/projectboard/controller/DataRestTest.java
@@ -0,0 +1,89 @@
+package io.mykim.projectboard.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@DisplayName("Data REST, HAL로 설정한 api 테스트")
+@Slf4j
+@AutoConfigureMockMvc // @SpringBootTest에서 MockMvc 객체를 사용하기 위함
+@SpringBootTest
+@Transactional  // 통합테스트
+public class DataRestTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("get api 테스트 - 게시글 리스트 조회")
+    void findArticlesApiTest() throws Exception{
+        // given
+        String api = "/api/articles";
+
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get(api))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().contentType((MediaType.valueOf("application/hal+json"))))
+                    .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("get api 테스트 - 게시글 단건 조회")
+    void findArticleByIdApiTest() throws Exception{
+        // given
+        String api = "/api/articles/{articleId}";
+
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get(api, 2))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType((MediaType.valueOf("application/hal+json"))))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("get api 테스트 - 게시글 -> 댓글 리스트 조회")
+    void findArticleCommentsInArticleApiTest() throws Exception{
+        // given
+        String api = "/api/articles/{articleId}/articleComments";
+
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get(api, 2))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType((MediaType.valueOf("application/hal+json"))))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("get api 테스트 - 전체 댓글 리스트 조회")
+    void findArticleCommentsApiTest() throws Exception{
+        // given
+        String api = "/api/articleComments";
+
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get(api))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType((MediaType.valueOf("application/hal+json"))))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("get api 테스트 - 댓글 단건 조회")
+    void findArticleCommentByIdApiTest() throws Exception{
+        // given
+        String api = "/api/articleComments/{articleCommentId}";
+
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get(api, 100))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType((MediaType.valueOf("application/hal+json"))))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
