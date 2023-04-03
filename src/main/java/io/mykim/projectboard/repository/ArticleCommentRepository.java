9a2f77bb2efa8e832cc670fc94cfe361b93c2aca
mykim <mykim8710@gmail.com>
2023-04-03 23:04:45 +0900

    #1 - Data Rest가 적용된 자동 api사용 안함
    Data Rest가 적용되어 자동으로 생성된 api를 사용안하고 직접 구현하기 위해서 설정들 disabled

--- src/main/java/io/mykim/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/io/mykim/projectboard/repository/ArticleCommentRepository.java
@@ -4,6 +4,6 @@ import io.mykim.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+//@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/io/mykim/projectboard/repository/ArticleRepository.java
+++ src/main/java/io/mykim/projectboard/repository/ArticleRepository.java
@@ -4,7 +4,7 @@ import io.mykim.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+//@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 
 }
