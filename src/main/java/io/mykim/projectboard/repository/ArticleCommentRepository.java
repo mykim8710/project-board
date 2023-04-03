11b48dcd59ff89a577eb21c31ff023410f952944
mykim <mykim8710@gmail.com>
2023-04-03 22:04:54 +0900

    #1 - data rest 설정
    Article, ArticleComment의 api를 자동으로 restful하게 생성되게끔 설정

--- src/main/java/io/mykim/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/io/mykim/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package io.mykim.projectboard.repository;
 
 import io.mykim.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
