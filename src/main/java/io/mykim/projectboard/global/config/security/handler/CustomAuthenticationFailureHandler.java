ee056e851c8fa8b98aca054696f5f5cb07ae42e4
mykim <mykim8710@gmail.com>
2023-04-18 20:58:45 +0900

    #28 - 로그인 페이지 기능구현
    - 로그인 실패에 대한 에러 메세지 출력기능 추가(Form 로그인방식)
    - 로그인 성공 및 인증성공에 대한 것은 스프링시큐리티 설정 부분과 함께 진행할 예정

--- src/main/java/io/mykim/projectboard/global/config/security/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/io/mykim/projectboard/global/config/security/handler/CustomAuthenticationFailureHandler.java
@@ -16,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @Slf4j
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
@@ -27,34 +28,36 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
         /**
          * form login 방식
          */
-//        String errorMessage = null;
-//
-//        // 해당계정이 없을때
-//        if (exception instanceof UsernameNotFoundException) {
-//            errorMessage = ErrorCode.NOT_FOUND_USER.getMessage();
-//        }
-//
-//        // 비밀번호가 틀릴때 BadCredentialsException < AuthenticationException < RuntimeException
-//        if (exception instanceof BadCredentialsException) {
-//            errorMessage = ErrorCode.NOT_MATCH_PASSWORD.getMessage();
-//        }
-//
-//        errorMessage= URLEncoder.encode(errorMessage,"UTF-8"); // 한글 인코딩 깨지는 문제방지
-//        setDefaultFailureUrl("/?error=true&exception=" +errorMessage);
-//        onAuthenticationFailure(request, response, exception);
+        String errorMessage = null;
+
+        // 해당계정이 없을때
+        if (exception instanceof UsernameNotFoundException) {
+            errorMessage = CustomErrorCode.NOT_FOUND_USER.getMessage();
+        }
+
+        // 비밀번호가 틀릴때 BadCredentialsException < AuthenticationException < RuntimeException
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = CustomErrorCode.NOT_MATCH_PASSWORD.getMessage();
+        }
+
+        errorMessage= URLEncoder.encode(errorMessage,"UTF-8"); // 한글 인코딩 깨지는 문제방지
+        setDefaultFailureUrl("/users/sign-in?error=true&exception=" +errorMessage);
+        super.onAuthenticationFailure(request, response, exception);
+
+
 
         /**
          * API login 방식
          */
         // 해당계정이 없을때
-        if (exception instanceof UsernameNotFoundException) {
+        /*if (exception instanceof UsernameNotFoundException) {
             sendErrorResponseApiLogin(response, CustomErrorCode.NOT_FOUND_USER);
         }
 
         // 비밀번호가 틀릴때 BadCredentialsException < AuthenticationException < RuntimeException
         if (exception instanceof BadCredentialsException) {
             sendErrorResponseApiLogin(response, CustomErrorCode.NOT_MATCH_PASSWORD);
-        }
+        }*/
     }
 
     // API login 방식
