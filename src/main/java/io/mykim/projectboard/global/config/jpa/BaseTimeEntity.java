6dc34fc7d452bbdbfa2493915bdf65e44a587c76
mykim8710 <mykim8710@gmail.com>
2023-04-20 18:53:30 +0900

    #31 - BaseTimeEntity 수정 및 분리 - 기존 At, By을 분리 - At은 Article, ArticleComment와 연관관계 매핑

--- src/main/java/io/mykim/projectboard/global/config/jpa/BaseTimeEntity.java
+++ src/main/java/io/mykim/projectboard/global/config/jpa/BaseTimeEntity.java
@@ -1,9 +1,7 @@
 package io.mykim.projectboard.global.config.jpa;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -19,16 +17,10 @@ import java.time.LocalDateTime;
 })
 public class BaseTimeEntity {
     @CreatedDate
-    @Column(nullable = false, name = "created_at")
+    @Column(nullable = false, name = "created_at", updatable = false)
     private LocalDateTime createdAt;
-    @CreatedBy
-    @Column(nullable = false, length = 100, name = "created_by")
-    private String createdBy;
+
     @LastModifiedDate
     @Column(nullable = false,  name = "last_modified_at")
     private LocalDateTime lastModifiedAt;
-    @LastModifiedBy
-    @Column(nullable = false, length = 100, name = "last_modified_by")
-    private String lastModifiedBy;
-
 }
