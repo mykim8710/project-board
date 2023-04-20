b75c6edae0e6e93ef43fe0d459f166963d352ea1
mykim8710 <mykim8710@gmail.com>
2023-04-20 18:57:31 +0900

    #31 - User - Article, ArticleComment 연관관계 매핑 및 auditing수정으로 인한 Querydsl 조회 로직 수정

--- src/main/java/io/mykim/projectboard/article/dto/response/ResponseArticleFindDto.java
+++ src/main/java/io/mykim/projectboard/article/dto/response/ResponseArticleFindDto.java
@@ -19,11 +19,11 @@ public class ResponseArticleFindDto {
     private String hashtag;
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/MM/dd HH:mm:ss")
     private LocalDateTime createdAt;
-    private String createdBy;
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/MM/dd HH:mm:ss")
     private LocalDateTime lastModifiedAt;
-    private String lastModifiedBy;
 
+    private Long userId;
+    private String nickname;
 
     private ResponseArticleFindDto(Article article) {
         this.id = article.getId();
@@ -31,9 +31,9 @@ public class ResponseArticleFindDto {
         this.content = article.getContent();
         this.hashtag = article.getHashtag();
         this.createdAt = article.getCreatedAt();
-        this.createdBy = article.getCreatedBy();
         this.lastModifiedAt = article.getLastModifiedAt();
-        this.lastModifiedBy = article.getLastModifiedBy();
+        this.userId = article.getCreatedBy().getId();
+        this.nickname = article.getCreatedBy().getNickname();
     }
 
     public static ResponseArticleFindDto of(Article article) {
@@ -41,14 +41,17 @@ public class ResponseArticleFindDto {
     }
 
     @QueryProjection
-    public ResponseArticleFindDto(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime lastModifiedAt, String lastModifiedBy) {
+    public ResponseArticleFindDto(Long id,
+                                  String title, String content, String hashtag,
+                                  LocalDateTime createdAt, LocalDateTime lastModifiedAt,
+                                  Long userId, String nickname) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
         this.createdAt = createdAt;
-        this.createdBy = createdBy;
         this.lastModifiedAt = lastModifiedAt;
-        this.lastModifiedBy = lastModifiedBy;
+        this.userId = userId;
+        this.nickname = nickname;
     }
 }
