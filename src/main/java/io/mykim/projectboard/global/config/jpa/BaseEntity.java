20741b5749a3de6e370b2c809155574b177e3504
mykim8710 <mykim8710@gmail.com>
2023-04-21 18:07:31 +0900

    #30 - Audit Entity 수정 및 Config 수정

--- src/main/java/io/mykim/projectboard/global/config/jpa/BaseEntity.java
+++ src/main/java/io/mykim/projectboard/global/config/jpa/BaseEntity.java
@@ -4,21 +4,17 @@ import io.mykim.projectboard.user.entity.User;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-@Table(indexes = {
-        @Index(columnList = "created_at"),
-        @Index(columnList = "last_modified_at")
-})
 public class BaseEntity extends BaseTimeEntity {
     @CreatedBy
     @ToString.Exclude
@@ -29,6 +25,6 @@ public class BaseEntity extends BaseTimeEntity {
     @LastModifiedBy
     @ToString.Exclude
     @ManyToOne
-    @JoinColumn(referencedColumnName = "user_id", name = "last_modified_by_user_id", nullable = false, updatable = false)
+    @JoinColumn(referencedColumnName = "user_id", name = "last_modified_by_user_id", nullable = false)
     private User lastModifiedBy;
 }
