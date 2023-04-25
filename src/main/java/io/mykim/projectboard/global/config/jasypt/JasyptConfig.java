ad62af264a9eed178ef24d3ac4189dbf2e3abe25
mykim <mykim8710@gmail.com>
2023-04-26 00:19:37 +0900

    main - application.yaml 암호화 수정
    - aws parameter store 적용 X - 좀더 공부가 필요
    - 대신 jasypt사용, secretKey는 외부에서 중비받는 방식으로 전환

---
+++ src/main/java/io/mykim/projectboard/global/config/jasypt/JasyptConfig.java
@@ -0,0 +1,41 @@
+package io.mykim.projectboard.global.config.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+    @Value("${jasypt.encryptor.secrete-key}")
+    private String secretKey;
+
+    @Value("${jasypt.encryptor.algorithm}")
+    private String algorithm;
+
+    @Value("${jasypt.encryptor.pool-size}")
+    private String poolSize;
+
+    @Value("${jasypt.encryptor.key-obtention-iterations}")
+    private String keyObtentionIterations;
+
+    @Bean
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(secretKey); // 암호화할 때 사용하는 키
+        config.setAlgorithm(algorithm);                                      // 암호화 알고리즘
+        config.setKeyObtentionIterations(keyObtentionIterations);            // 반복할 해싱 회수
+        config.setPoolSize(poolSize);                                        // 인스턴스 pool
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
