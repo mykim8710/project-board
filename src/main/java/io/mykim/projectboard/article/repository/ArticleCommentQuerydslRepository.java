4d9346fad1713f11e141bbbc8b873cc5f7052c21
mykim8710 <mykim8710@gmail.com>
2023-05-02 17:37:49 +0900

    #53 - package재분류로인한 클래스파일 이동2 - querydsl용 repository는 따로 package로 묶음

--- src/main/java/io/mykim/projectboard/article/repository/ArticleCommentQuerydslRepository.java
+++
@@ -1,9 +0,0 @@
-package io.mykim.projectboard.article.repository;
-
-import io.mykim.projectboard.article.dto.response.ResponseArticleCommentFindDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ArticleCommentQuerydslRepository {
-    Page<ResponseArticleCommentFindDto> findAllArticleCommentUnderArticle(Pageable pageable, Long articleId);
-}
