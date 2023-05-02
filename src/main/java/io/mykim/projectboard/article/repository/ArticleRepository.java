e73114073c5c58b29473ce591a3c0820214679c0
mykim8710 <mykim8710@gmail.com>
2023-05-02 17:41:33 +0900

    #53 - package재분류로인한 클래스파일 이동3 - querydsl용 repository는 따로 package로 묶음

--- src/main/java/io/mykim/projectboard/article/repository/ArticleRepository.java
+++ src/main/java/io/mykim/projectboard/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package io.mykim.projectboard.article.repository;
 
 import io.mykim.projectboard.article.entity.Article;
+import io.mykim.projectboard.article.repository.querydsl.ArticleQuerydslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //@RepositoryRestResource
